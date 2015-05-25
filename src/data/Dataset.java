ee7e7bee69904dd3cef2b935035e8991cce18bcc
Enpeng Yao <yepyao@gmail.com>
2015-05-25 17:13:02 +0800

    =.=

--- src/data/Dataset.java
+++ src/data/Dataset.java
@@ -17,16 +17,25 @@ public class Dataset {
 	LinkedList<CheckIn> list = new LinkedList<CheckIn>();
 	HashMap<Integer, LinkedList<CheckIn>> userList = new HashMap<Integer, LinkedList<CheckIn>>();
 	HashMap<String, LinkedList<CheckIn>> locList = new HashMap<String, LinkedList<CheckIn>>();
+	int firstHour;
+	int lastHour;
+
 	public Dataset(String filename) throws Exception {
 		BufferedReader in = new BufferedReader(new FileReader(filename));
 		String line = null;
-		while ((line = in.readLine())!=null){
+		while ((line = in.readLine()) != null) {
 			CheckIn check = new CheckIn(line);
 			list.add(check);
 		}
 		in.close();
 		Collections.sort(list);
-		for(CheckIn check:list){
+		System.out.println("first time: " + list.getFirst().time.getTime()
+				/ 1000 / 60 / 60);
+		System.out.println("last time: " + list.getLast().time.getTime() / 1000
+				/ 60 / 60);
+		firstHour = (int) (list.getFirst().time.getTime() / 1000 / 60 / 60);
+		lastHour = (int) (list.getLast().time.getTime() / 1000 / 60 / 60);
+		for (CheckIn check : list) {
 			if (!userList.containsKey(check.userId))
 				userList.put(check.userId, new LinkedList<CheckIn>());
 			userList.get(check.userId).add(check);
@@ -35,53 +44,73 @@ public class Dataset {
 			locList.get(check.locId).add(check);
 		}
 		Set<Integer> userSet = new HashSet<Integer>(userList.keySet());
-		for(Integer userId:userSet){
-			if (userList.get(userId).size()<5)
+		for (Integer userId : userSet) {
+			if (userList.get(userId).size() < 5)
 				userList.remove(userId);
 		}
 		Set<String> locSet = new HashSet<String>(locList.keySet());
-		for(String locId:locSet){
-			if (locList.get(locId).size()<5)
+		for (String locId : locSet) {
+			if (locList.get(locId).size() < 5)
 				locList.remove(locId);
 		}
 		Iterator<CheckIn> iter = list.iterator();
-		while(iter.hasNext()){
+		while (iter.hasNext()) {
 			CheckIn check = iter.next();
-			if (!userList.containsKey(check.userId)||!locList.containsKey(check.locId))
+			if (!userList.containsKey(check.userId)
+					|| !locList.containsKey(check.locId))
 				iter.remove();
 		}
-		System.out.println("#checkin: "+list.size());
-		System.out.println("#User: "+userList.keySet().size());
-		System.out.println("#Loc: "+locList.keySet().size());
-		//divide
+		System.out.println("#checkin: " + list.size());
+		System.out.println("#User: " + userList.keySet().size());
+		System.out.println("#Loc: " + locList.keySet().size());
+		// divide
 		Random rand = new Random(0);
-		for(CheckIn check: list){
+		for (CheckIn check : list) {
 			int r = rand.nextInt(8);
-			if (r == 6 || r == 7) check.type = DataType.TEST;
-			if (r == 5) check.type = DataType.TUNE;
+			if (r == 6 || r == 7)
+				check.type = DataType.TEST;
+			if (r == 5)
+				check.type = DataType.TUNE;
 		}
 	}
-	public void outputUserPoints(String filename) throws IOException{
+
+	public void outputUserPoints(String filename) throws IOException {
 		PrintStream out = new PrintStream(filename);
-		for(Integer userId: userList.keySet()){
+		for (Integer userId : userList.keySet()) {
 			out.println(userId);
-			for(CheckIn check:userList.get(userId))
-				out.println(check.latitude+", "+check.longitude);
+			for (CheckIn check : userList.get(userId))
+				out.println(check.latitude + ", " + check.longitude);
 			out.println("------------------");
 		}
 		out.close();
 		System.out.println("outputUserPoints ok!");
 	}
-	
-	//get
-	public Set<Integer> getUserSet(){
+
+	// get
+	public Set<Integer> getUserSet() {
 		return userList.keySet();
 	}
-	public LinkedList<CheckIn> getUserCheckIns(int userId, DataType type){
+
+	public LinkedList<CheckIn> getUserCheckIns(int userId, DataType type) {
 		LinkedList<CheckIn> res = new LinkedList<CheckIn>();
-		for(CheckIn check:userList.get(userId)){
-			if (check.type == type) res.add(check);
+		for (CheckIn check : userList.get(userId)) {
+			if (check.type == type)
+				res.add(check);
 		}
 		return res;
+	}
+
+	public HashSet<String> getPosSet(LinkedList<CheckIn> checkIns, int hour) {
+		HashSet<String> res = new HashSet<String>();
+		for (CheckIn check : checkIns) {
+			if (check.time.getTime() / 1000 / 60 / 60 %24 == hour)
+				res.add(check.locId);
+		}
+		return res;
+	}
+
+	public int getHourLength() {
+		return 24;
+		//return lastHour - firstHour + 1;
 	}
 }
--- src/main/Evaluation.java
+++ src/main/Evaluation.java
@@ -22,57 +22,91 @@ public class Evaluation {
 		double rec5 = 0;
 		double rec10 = 0;
 		double rec20 = 0;
-		for(int userId: userSet){
-			double pos5 = 0;
-			double pos10 = 0;
-			double pos20 = 0;
-			LinkedList<CheckIn> checkIns = dataset.getUserCheckIns(userId, DataType.TEST);
-			HashSet<String> posSet = new HashSet<String>();
-			for(CheckIn check: checkIns) posSet.add(check.locId);
-			
-			HashMap<String, Double> pred = res.get(userId);
-			ArrayList<PredPoint> list = new ArrayList<PredPoint>();
-			for(String locId:pred.keySet()){
-				list.add(new PredPoint(locId, pred.get(locId)));
-			}
-			Collections.sort(list);
-			for(int i=0;i<20;i++){
-				String locId = list.get(i).locId;
-				if (posSet.contains(locId)){
-					if (i<5) pos5++;
-					if (i<10) pos10++;
-					if (i<20) pos20++;
+
+		int hours = dataset.getHourLength();
+		int validHours = 0;
+		for (int h = 0; h < hours; h++) {
+			double tp5 = 0;
+			double tp10 = 0;
+			double tp20 = 0;
+
+			double fp5 = 0;
+			double fp10 = 0;
+			double fp20 = 0;
+
+			double fn5 = 0;
+			double fn10 = 0;
+			double fn20 = 0;
+			for (int userId : userSet) {
+				HashMap<String, Double> pred = res.get(userId);
+				ArrayList<PredPoint> list = new ArrayList<PredPoint>();
+				for (String locId : pred.keySet()) {
+					list.add(new PredPoint(locId, pred.get(locId)));
 				}
+				Collections.sort(list);
+
+				LinkedList<CheckIn> checkIns = dataset.getUserCheckIns(userId,
+						DataType.TEST);
+				HashSet<String> posSet = dataset.getPosSet(checkIns, h);
+				if (posSet.size() == 0)
+					continue;
+				validHours++;
+
+				for (int i = 0; i < 20; i++) {
+					String locId = list.get(i).locId;
+					if (posSet.contains(locId)) {
+						if (i < 5)
+							tp5++;
+						if (i < 10)
+							tp10++;
+						if (i < 20)
+							tp20++;
+					}
+				}
+				fp5 += 5 - tp5;
+				fp10 += 10 - tp10;
+				fp20 += 20 - tp20;
+
+				fn5 += posSet.size() - tp5;
+				fn10 += posSet.size() - tp10;
+				fn20 += posSet.size() - tp20;
+
 			}
-			pre5 += pos5/5;
-			pre10 += pos10/10;
-			pre20 += pos20/20;
-			
-			rec5 += pos5/posSet.size();
-			rec10 += pos10/posSet.size();
-			rec20 += pos20/posSet.size();
+
+			pre5 += tp5 / (tp5 + fp5);
+			pre10 += tp10 / (tp10 + fp10);
+			pre20 += tp20 / (tp20 + fp20);
+
+			rec5 += tp5 / (tp5 + fn5);
+			rec10 += tp10 / (tp10 + fn10);
+			rec20 += tp20 / (tp20 + fn20);
 		}
-		int userNum = userSet.size();
-		System.out.println("pre 5:"+pre5/userNum);
-		System.out.println("pre10:"+pre10/userNum);
-		System.out.println("pre20:"+pre20/userNum);
-		System.out.println("rec 5:"+rec5/userNum);
-		System.out.println("rec10:"+rec10/userNum);
-		System.out.println("rec20:"+rec20/userNum);
+
+		// int userNum = userSet.size();
+		System.out.println("pre 5:" + pre5 / validHours);
+		System.out.println("pre10:" + pre10 / validHours);
+		System.out.println("pre20:" + pre20 / validHours);
+		System.out.println("rec 5:" + rec5 / validHours);
+		System.out.println("rec10:" + rec10 / validHours);
+		System.out.println("rec20:" + rec20 / validHours);
 	}
 
 }
-class PredPoint implements Comparable<PredPoint>{
+
+class PredPoint implements Comparable<PredPoint> {
 	String locId;
 	double res;
+
 	public PredPoint(String locId, double res) {
 		this.locId = locId;
 		this.res = res;
 	}
+
 	@Override
 	public int compareTo(PredPoint o) {
-		if (res == o.res) return 0;
-		return (res>o.res)?-1:1;
+		if (res == o.res)
+			return 0;
+		return (res > o.res) ? -1 : 1;
 	}
-	
+
 }
